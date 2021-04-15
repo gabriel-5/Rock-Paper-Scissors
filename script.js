6e95a826beedd86c00e4a3b35d7c9436e4533ecd
gospelship <gabemanzi@gmail.com>
2021-04-15 18:26:02 +0100

    Random selection working

---
+++ script.js
@@ -0,0 +1,7 @@
+function computerPlay(){
+    let possMoves = ["Rock", "Paper", "Scissors"];
+    let randomPick = possMoves [Math.floor(Math.random() * possMoves.length)];
+    console.log(randomPick)
+
+}
+
