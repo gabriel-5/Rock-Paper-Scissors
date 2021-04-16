82e26fbfe23f9c4d003b52e66a08b97fca24261a
gospelship <gabemanzi@gmail.com>
2021-04-16 12:05:28 +0100

    Working round without player input

---
+++ oldscript.js
@@ -0,0 +1,40 @@
+
+
+/*function computerPlay(){
+    let possMoves = ["Rock", "Paper", "Scissors"];
+    let randomPick = possMoves [Math.floor(Math.random() * possMoves.length)];
+    console.log(randomPick);
+    return(randomPick);
+
+}
+
+function playRound(playerSelection, computerSelection){
+    
+    let result;
+    if (computerSelection = "Paper") {
+        result = "you've lost it";
+    } else if (computerSelection = "Scissors") {
+        result = "you've won it";
+    } else {
+        result = "you've drawn it";
+    }
+    console.log("this is where the result would be");
+}
+
+
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+//console.log("test string")
+
+*/
+
+function alertFunct(){
+    alert("test")
+    console.log("console test");
+}
+
+//alertFunct();
+
+
--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function computerPlay(){
+const computerSelection = computerPlay(); //here the function is called as it is placed into the variable computerSelection
+const playerSelection = "rock";
+
+function computerPlay(){ // this function chooses a random move for the computer
     let possMoves = ["Rock", "Paper", "Scissors"];
     let randomPick = possMoves [Math.floor(Math.random() * possMoves.length)];
-    console.log(randomPick)
+    console.log(randomPick);
+    return(randomPick);
 
 }
+
+/*function playRound(playerSelection, computerSelection){ //currently this function takes the return value of the computerPlay function as uses it as an argument to find out what the computer played
+    if (computerSelection === "Scissors"){
+        alert ("it's a scissors");
+    } else if (computerSelection === "Paper"){
+        alert ("it's a paper");
+    } else {
+        alert ("it's a rock");
+    }
+
+}*/
+
+function playRound(playerSelection, computerSelection){ 
+    if (playerSelection === "rock" && computerSelection === "Rock"){
+        alert ("It's a draw!);
+    } else if (playerSelection === "rock" && computerSelection === "Paper") {
+        alert ("You lose!");
+    } else if (playerSelection === "rock" && computerSelection === "Scissors"){
+        alert ("You win!");
+    }
+}
+playRound(playerSelection, computerSelection);
+
 
