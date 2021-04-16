1be5244bf0c718d6edd22f26bdd141d50dd725aa
gospelship <gabemanzi@gmail.com>
2021-04-16 13:26:22 +0100

    Working with player choice, rock only

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const computerSelection = computerPlay(); //here the function is called as it is placed into the variable computerSelection
-const playerSelection = "rock";
+
 
 function computerPlay(){ // this function chooses a random move for the computer
     let possMoves = ["Rock", "Paper", "Scissors"];
@@ -11,13 +11,15 @@ function computerPlay(){ // this function chooses a random move for the computer
 
 function playRound(playerSelection, computerSelection){ 
     if (playerSelection === "rock" && computerSelection === "Rock"){
-        alert ("It's a draw!);
+        alert ("It's a draw!");
     } else if (playerSelection === "rock" && computerSelection === "Paper") {
         alert ("You lose!");
     } else if (playerSelection === "rock" && computerSelection === "Scissors"){
         alert ("You win!");
     }
 }
+
+const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
 playRound(playerSelection, computerSelection);
 
 
