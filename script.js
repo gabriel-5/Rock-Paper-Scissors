bb28d480844b1001a3583640f6694148a3739ce3
gospelship <gabemanzi@gmail.com>
2021-04-16 14:11:01 +0100

    Saving changes with one round working before reworking function structure

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const computerSelection = computerPlay(); //here the function is called as it is placed into the variable computerSelection
 
 let draw = 0
 let win = 0
@@ -12,43 +11,47 @@ function computerPlay(){ // this function chooses a random move for the computer
 
 }
 
-function playRound(playerSelection, computerSelection){
-   
+function playRound(playerSelection, computerSelection){ //the variables playerSelection and computerSelection are used as arguments for the function. based on the outcome, the win/lose/draw variables are changed to get the current score
     
     if (playerSelection === "rock" && computerSelection === "Rock"){
         draw = (draw + 1);
-        alert ("It's a draw!");   
+        alert ("It's a draw! You currently have " + win + " win " + draw + " draw, and " + lose + " loss") ;   
     } else if (playerSelection === "rock" && computerSelection === "Paper") {
         lose = (lose + 1);
-        alert ("You lose! Paper covers rock.");    
+        alert ("You lose! Paper covers rock. You currently have " + win + " win " + draw + " draw, and " + lose + " loss");    
     } else if (playerSelection === "rock" && computerSelection === "Scissors"){
         win = (win + 1);
-        alert ("You win! Rock breaks scissors.");
+        alert ("You win! Rock breaks scissors. You currently have " + win + " win " + draw + " draw, and " + lose + " loss");
     }
       else if (playerSelection === "scissors" && computerSelection === "Rock"){
         lose = (lose + 1);
-        alert ("You lose! Rock breaks scissors.");
+        alert ("You lose! Rock breaks scissors. You currently have " + win + " win " + draw + " draw, and " + lose + " loss");
     } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         win = (win + 1);
-        alert ("You win! Scissors cut paper.");       
+        alert ("You win! Scissors cut paper. You currently have " + win + " win " + draw + " draw, and " + lose + " loss");       
     } else if (playerSelection === "scissors" && computerSelection === "Scissors"){
         draw = (draw + 1);
-        alert ("It's a draw!");
+        alert ("It's a draw! You currently have " + win + " win " + draw + " draw, and " + lose + " loss");
     }
     else if (playerSelection === "paper" && computerSelection === "Rock"){
         win = (win + 1);
-        alert ("You win! Paper covers rock."); 
+        alert ("You win! Paper covers rock. You currently have " + win + " win " + draw + " draw, and " + lose + " loss"); 
     } else if (playerSelection === "paper" && computerSelection === "Paper") {
         draw = (draw + 1);
-        alert ("It's a draw!");
+        alert ("It's a draw! You currently have " + win + " win " + draw + " draw, and " + lose + " loss");
     } else if (playerSelection === "paper" && computerSelection === "Scissors"){
         lose = (lose + 1);
-        alert ("You lose! Scissors cut paper.");
+        alert ("You lose! Scissors cut paper. You currently have " + win + " win " + draw + " draw, and " + lose + " loss");
         
     }
 }
 
-const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+const computerSelection = computerPlay();  // the function computerPlay is called and its return value is placed into the variable computerSelection
+const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase(); // the player is prompted to type either r/p/s and their input is made lowercase and put into the variable playerselection
 playRound(playerSelection, computerSelection);
+
+function game(){
+    
+}
 
 
