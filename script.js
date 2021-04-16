dc040628ba300fbb21d1cb923157173ef698903c
gospelship <gabemanzi@gmail.com>
2021-04-16 13:39:24 +0100

    Game working with score count for 1 round

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const computerSelection = computerPlay(); //here the function is called as it is placed into the variable computerSelection
 
+let draw = 0
+let win = 0
+let lose = 0
 
 function computerPlay(){ // this function chooses a random move for the computer
     let possMoves = ["Rock", "Paper", "Scissors"];
@@ -9,13 +12,39 @@ function computerPlay(){ // this function chooses a random move for the computer
 
 }
 
-function playRound(playerSelection, computerSelection){ 
+function playRound(playerSelection, computerSelection){
+   
+    
     if (playerSelection === "rock" && computerSelection === "Rock"){
-        alert ("It's a draw!");
+        draw = (draw + 1);
+        alert ("It's a draw!");   
     } else if (playerSelection === "rock" && computerSelection === "Paper") {
-        alert ("You lose!");
+        lose = (lose + 1);
+        alert ("You lose! Paper covers rock.");    
     } else if (playerSelection === "rock" && computerSelection === "Scissors"){
-        alert ("You win!");
+        win = (win + 1);
+        alert ("You win! Rock breaks scissors.");
+    }
+      else if (playerSelection === "scissors" && computerSelection === "Rock"){
+        lose = (lose + 1);
+        alert ("You lose! Rock breaks scissors.");
+    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        win = (win + 1);
+        alert ("You win! Scissors cut paper.");       
+    } else if (playerSelection === "scissors" && computerSelection === "Scissors"){
+        draw = (draw + 1);
+        alert ("It's a draw!");
+    }
+    else if (playerSelection === "paper" && computerSelection === "Rock"){
+        win = (win + 1);
+        alert ("You win! Paper covers rock."); 
+    } else if (playerSelection === "paper" && computerSelection === "Paper") {
+        draw = (draw + 1);
+        alert ("It's a draw!");
+    } else if (playerSelection === "paper" && computerSelection === "Scissors"){
+        lose = (lose + 1);
+        alert ("You lose! Scissors cut paper.");
+        
     }
 }
 
