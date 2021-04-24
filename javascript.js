529d77e454bf56dbd3c83dfade8778cbb6b00618
Gabriel M <gabe.manzi@gmail.com>
2021-04-24 09:57:37 +0100

    Single round works with button press, design updated

--- javascript.js
+++ javascript.js
@@ -1,55 +1,61 @@
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
 
-function computerPlay(){ // Chooses a random move for the computer.
-    let possMoves = ["Rock", "Paper", "Scissors"]; // Array containing the three possible moves
-    let randomPick = possMoves [Math.floor(Math.random() * possMoves.length)]; // Radomly selects one of the three moves
-    //console.log(randomPick); //Logs the choice to console to check if it's working
-    return(randomPick); // Returns the random choice to be stored in a variable when the function is called
 
-}
-function userPlay(){ // Prompts the player to make a choice
-   return prompt("Rock, Paper or Scissors?").toLowerCase();
+function computerPlay(){
+    let possMoves = ["Rock", "Paper", "Scissors"]; 
+    let randomPick = possMoves [Math.floor(Math.random() * possMoves.length)]; 
+    console.log(randomPick);
+    return(randomPick); 
 }
 
-let computerSelection  = computerPlay;
-let playerSelection = userPlay;
 
-function playRound(playerSelection, computerSelection){ // Takes the variables containing the player and computer moves and figures out a winner.
+function playRound(playerSelection){ 
 
+    computerSelection = computerPlay();
 
     if (playerSelection === "rock" && computerSelection === "Rock"){
+        console.log("It's a draw: DOUBLE ROCK");
         return "It's a draw!";   
     } else if (playerSelection === "rock" && computerSelection === "Paper") {
         computerScore = (computerScore + 1);
+        console.log("You lose! PAPER COVERS ROCK");
         return "You lose! Paper covers rock.";    
     } else if (playerSelection === "rock" && computerSelection === "Scissors"){
         playerScore = (playerScore + 1);
+        console.log("You win! ROCK BREAKS SCISSORS");
         return "You win! Rock breaks scissors.";
     }
       else if (playerSelection === "scissors" && computerSelection === "Rock"){
         computerScore = (computerScore + 1);
+        console.log( "You lose! Rock breaks scissors.");
         return "You lose! Rock breaks scissors.";
     } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         playerScore = (playerScore + 1);
+        console.log( "You win! SCISSORS CUT PAPER");
         return "You win! Scissors cut paper.";       
     } else if (playerSelection === "scissors" && computerSelection === "Scissors"){
+        console.log( "It's a draw: DOUBLE SCISSORS");
         return "It's a draw!";
     }
     else if (playerSelection === "paper" && computerSelection === "Rock"){
         playerScore = (playerScore + 1);
+        console.log("You win! PAPER COVERS ROCK");
         return "You win! Paper covers rock."; 
     } else if (playerSelection === "paper" && computerSelection === "Paper") {
+        console.log("It's a draw: DOUBLE PAPER");
         return "It's a draw!";
     } else if (playerSelection === "paper" && computerSelection === "Scissors"){
         computerScore = (computerScore + 1);
+        console.log("You lose! SCISSORS CUT PAPER");
         return "You lose! Scissors cut paper.";
         
     }
 
 }
 
-function whoWins(){
+function result(){
     if (playerScore > computerScore){
         console.log("You win! You got " + playerScore + " points and the computer got " + computerScore + " points.");
     } else if (playerScore < computerScore){
@@ -60,18 +66,20 @@ function whoWins(){
 
 }
 
-function game(){
-    //for(i = 0; i < 5; i++){
-        computerSelection = computerPlay();
-        playerSelection = userPlay()
-        console.log(playRound(playerSelection, computerSelection));
-    
-    whoWins();
-  
-   
-}
 
-game();
+const rockButton = document.querySelector("#rock")
+const paperButton = document.querySelector("#paper")
+const scissorsButton = document.querySelector("#scissors")
+
+rockButton.addEventListener("click", e => {
+    playRound("rock");
+})
+paperButton.addEventListener("click", e => {
+    playRound("paper");
+})
+scissorsButton.addEventListener("click", e => {
+    playRound("scissors");
+})
 
 
 
