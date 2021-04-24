d7de18b1791463b05b89ec18c7eafdda9a8cf252
Gabriel M <gabe.manzi@gmail.com>
2021-04-24 11:15:27 +0100

    Working with score counter

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
 let playerSelection;
+let textBox = document.querySelector("#text");
+let scoreCount = document.querySelector("#score");
+
+
 
 
 function computerPlay(){
@@ -11,44 +15,55 @@ function computerPlay(){
 }
 
 
+
+
 function playRound(playerSelection){ 
 
     computerSelection = computerPlay();
 
     if (playerSelection === "rock" && computerSelection === "Rock"){
-        console.log("It's a draw: DOUBLE ROCK");
+        textBox.textContent = "It's a draw: DOUBLE ROCK";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "It's a draw!";   
     } else if (playerSelection === "rock" && computerSelection === "Paper") {
         computerScore = (computerScore + 1);
-        console.log("You lose! PAPER COVERS ROCK");
+        textBox.textContent = "You lose! PAPER COVERS ROCK";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "You lose! Paper covers rock.";    
     } else if (playerSelection === "rock" && computerSelection === "Scissors"){
         playerScore = (playerScore + 1);
-        console.log("You win! ROCK BREAKS SCISSORS");
+        textBox.textContent = "You win! ROCK BREAKS SCISSORS";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "You win! Rock breaks scissors.";
     }
       else if (playerSelection === "scissors" && computerSelection === "Rock"){
         computerScore = (computerScore + 1);
-        console.log( "You lose! Rock breaks scissors.");
+        textBox.textContent = "You lose! ROCK BREAKS SCISSORS";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "You lose! Rock breaks scissors.";
     } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         playerScore = (playerScore + 1);
-        console.log( "You win! SCISSORS CUT PAPER");
+        textBox.textContent = "You win! SCISSORS CUT PAPER";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "You win! Scissors cut paper.";       
     } else if (playerSelection === "scissors" && computerSelection === "Scissors"){
-        console.log( "It's a draw: DOUBLE SCISSORS");
+        textBox.textContent = "It's a draw: DOUBLE SCISSORS";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "It's a draw!";
     }
     else if (playerSelection === "paper" && computerSelection === "Rock"){
         playerScore = (playerScore + 1);
-        console.log("You win! PAPER COVERS ROCK");
+        textBox.textContent = "You win! PAPER COVERS ROCK";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "You win! Paper covers rock."; 
     } else if (playerSelection === "paper" && computerSelection === "Paper") {
-        console.log("It's a draw: DOUBLE PAPER");
+        textBox.textContent = "It's a draw: DOUBLE PAPER";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "It's a draw!";
     } else if (playerSelection === "paper" && computerSelection === "Scissors"){
         computerScore = (computerScore + 1);
-        console.log("You lose! SCISSORS CUT PAPER");
+        textBox.textContent = "You lose! SCISSORS CUT PAPER";
+        scoreCount.textContent = "Your score: " + playerScore + " Opponent Score:" + computerScore;
         return "You lose! Scissors cut paper.";
         
     }
